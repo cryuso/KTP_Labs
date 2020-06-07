393321b4385b34080763c8c4031ed320970f1cfa
Irina Kuranova <47595007+cryuso@users.noreply.github.com>
2020-06-07 23:35:35 +0300

    Create AStarApp.java

---
+++ Laboratory_3/AStarApp.java
@@ -0,0 +1,221 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+/**
+* Юзеру предоставляется карта, содержащая начальное и конечное местоположение. Юзер
+* может нарисовать или очистить препятствия на карте, а затем нажать кнопку, чтобы вычислить
+* путь от начала до конца, используя алгоритм поиска пути A*. Если путь
+* найден, он отобразится зеленым цветом.
+**/
+public class AStarApp {
+    /** количество ячеек в направлении x **/
+    private int width;
+    /** количество ячеек в направлении y **/
+    private int height;
+    /** место начала пути **/
+    private Location startLoc;
+    /** место, где путь должен закончиться **/
+    private Location finishLoc;
+    
+    /** 
+    * Двумерный массив компонентов пользовательского интерфейса, 
+    * которые обеспечивают отображение и обработку ячеек на карте.
+    **/
+     
+    private JMapCell[][] mapCells;
+
+    /**
+     * Этот внутренний класс обрабатывает события в основной сетке ячеек карты,
+     * изменение ячеек на основе состояния кнопки мыши и начального редактирования, 
+     * которое было выполнено.
+     **/
+    private class MapCellHandler implements MouseListener
+    {
+        /**
+        * Это значение будет истинным, если была нажата кнопка мыши, 
+        * и в данный момент мы выполняем операцию модификации.
+         **/
+        private boolean modifying;
+
+        /**
+        * Это значение записывает, делаем ли мы ячейки проходимыми или
+        * непроходимыми. Зависит от исходного состояния ячейки, в которой была запущена операция.
+         **/
+        private boolean makePassable;
+
+        /** Инициилизация операции модификации **/
+        public void mousePressed(MouseEvent e)
+        {
+            modifying = true;
+
+            JMapCell cell = (JMapCell) e.getSource();
+
+            // Если текущая ячейка проходима, то мы делаем ее
+            // непроходимой; если текущая ячейка непроходима, то делаем проходимой.
+
+            makePassable = !cell.isPassable();
+
+            cell.setPassable(makePassable);
+        }
+
+        /** Конец операции модификации **/
+        public void mouseReleased(MouseEvent e)
+        {
+            modifying = false;
+        }
+
+        /**
+         * Если мышь была нажата, то операция модификации продолжится в новой ячейке.
+         **/
+        public void mouseEntered(MouseEvent e)
+        {
+            if (modifying)
+            {
+                JMapCell cell = (JMapCell) e.getSource();
+                cell.setPassable(makePassable);
+            }
+        }
+
+        /**Не требуется для этого обработчика**/
+        public void mouseExited(MouseEvent e)
+        {
+            //игнорируем
+        }
+
+        /**Не требуется для этого обработчика**/
+        public void mouseClicked(MouseEvent e)
+        {
+            //это тоже игнорируем
+        }
+    }
+
+
+    /**
+     * Создается новый экземпляр AStarApp с указанными длиной и шириной карты.
+     **/
+    public AStarApp(int w, int h) {
+        if (w <= 0)
+            throw new IllegalArgumentException("w must be > 0; got " + w);
+
+        if (h <= 0)
+            throw new IllegalArgumentException("h must be > 0; got " + h);
+
+        width = w;
+        height = h;
+
+        startLoc = new Location(2, h / 2);
+        finishLoc = new Location(w - 3, h / 2);
+    }
+
+
+    private void initGUI()
+    {
+        JFrame frame = new JFrame("Pathfinder");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Container contentPane = frame.getContentPane();
+
+        contentPane.setLayout(new BorderLayout());
+
+        GridBagLayout gbLayout = new GridBagLayout();
+        GridBagConstraints gbConstraints = new GridBagConstraints();
+        gbConstraints.fill = GridBagConstraints.BOTH;
+        gbConstraints.weightx = 1;
+        gbConstraints.weighty = 1;
+        gbConstraints.insets.set(0, 0, 1, 1);
+
+        JPanel mapPanel = new JPanel(gbLayout);
+        mapPanel.setBackground(Color.GRAY);
+
+        mapCells = new JMapCell[width][height];
+
+        MapCellHandler cellHandler = new MapCellHandler();
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                mapCells[x][y] = new JMapCell();
+
+                gbConstraints.gridx = x;
+                gbConstraints.gridy = y;
+
+                gbLayout.setConstraints(mapCells[x][y], gbConstraints);
+
+                mapPanel.add(mapCells[x][y]);
+                mapCells[x][y].addMouseListener(cellHandler);
+            }
+        }
+
+        contentPane.add(mapPanel, BorderLayout.CENTER);
+
+        JButton findPathButton = new JButton("Find Path");
+        findPathButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) { findAndShowPath(); }
+        });
+
+        contentPane.add(findPathButton, BorderLayout.SOUTH);
+
+        frame.pack();
+        frame.setVisible(true);
+
+        mapCells[startLoc.xCoord][startLoc.yCoord].setEndpoint(true);
+        mapCells[finishLoc.xCoord][finishLoc.yCoord].setEndpoint(true);
+    }
+
+
+    /** Старт приложения **/
+    private void start()
+    {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() { initGUI(); }
+        });
+    }
+
+
+    /**
+    * Этот вспомогательный метод пытается вычислить путь, используя текущее состояние карты. 
+     **/
+    private void findAndShowPath()
+    {
+
+        Map2D map = new Map2D(width, height);
+        map.setStart(startLoc);
+        map.setFinish(finishLoc);
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                mapCells[x][y].setPath(false);
+
+                if (mapCells[x][y].isPassable())
+                    map.setCellValue(x, y, 0);
+                else
+                    map.setCellValue(x, y, Integer.MAX_VALUE);
+            }
+        }
+
+        // Пробуем вычислить путь. Если вычисляется, помечаем все ячейки
+        // пути.
+
+        Waypoint wp = AStarPathfinder.computePath(map);
+
+        while (wp != null)
+        {
+            Location loc = wp.getLocation();
+            mapCells[loc.xCoord][loc.yCoord].setPath(true);
+
+            wp = wp.getPrevious();
+        }
+    }
+
+
+    /**
+     * Точка входа
+     **/
+    public static void main(String[] args) {
+        AStarApp app = new AStarApp(40, 30);
+        app.start();
+    }
+}
