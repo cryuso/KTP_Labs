21b77e645528ace81442febe8d358d255a24d5ec
Irina Kuranova <47595007+cryuso@users.noreply.github.com>
2020-06-08 13:32:51 +0300

    Update AStarPathfinder.java

--- Laboratory_3/AStarPathfinder.java
+++ Laboratory_3/AStarPathfinder.java
@@ -76,53 +76,49 @@ public class AStarPathfinder
             {
                 Location nextLoc = new Location(x, y);
 
-                // If "next location" is outside the map, skip it.
+                // Если "следующее местоположение" находится за пределами карты, пропускаем его
                 if (!map.contains(nextLoc))
                     continue;
 
-                // If "next location" is this location, skip it.
+                // Если «следующее местоположение» - текщее местоположение, пропускаем его
                 if (nextLoc == loc)
                     continue;
 
-                // If this location happens to already be in the "closed" set
-                // then continue on with the next location.
+                // Если это местоположение уже находится в "закрытом" наборе, 
+                // тогда переходим к "следующему местоположению"
                 if (state.isLocationClosed(nextLoc))
                     continue;
 
-                // Make a waypoint for this "next location."
+                // Делаем путевую точку для "следующего местоположения"
 
                 Waypoint nextWP = new Waypoint(nextLoc, currWP);
 
-                // OK, we cheat and use the cost estimate to compute the actual
-                // cost from the previous cell.  Then, we add in the cost from
-                // the map cell we step onto, to incorporate barriers etc.
-
+                // "Обманываем" и используем оценку стоимости, чтобы вычислить фактическую стоимость из предыдущей ячейки. 
+                // Затем мы добавляем стоимость из ячейки карты, на которую мы наступаем, для включения барьеров и тд
                 float prevCost = currWP.getPreviousCost() +
                         estimateTravelCost(currWP.getLocation(),
                                 nextWP.getLocation());
 
                 prevCost += map.getCellValue(nextLoc);
 
-                // Skip this "next location" if it is too costly.
+                // Пропускаем "следующее местоположение", если оно "слишком дорогое"
                 if (prevCost >= COST_LIMIT)
                     continue;
 
                 nextWP.setCosts(prevCost,
                         estimateTravelCost(nextLoc, map.getFinish()));
 
-                // Add the waypoint to the set of open waypoints.  If there
-                // happens to already be a waypoint for this location, the new
-                // waypoint only replaces the old waypoint if it is less costly
-                // than the old one.
+                // Добавляем путевую точку в набор "открытых" путевых точек. 
+                // Если для этого местоположения уже есть путевая точка, новая путевая точка заменяет эту старую путевую точку, 
+                // если она "дешевле", чем старая.
                 state.addOpenWaypoint(nextWP);
             }
         }
     }
 
     /**
-     * Estimates the cost of traveling between the two specified locations.
-     * The actual cost computed is just the straight-line distance between the
-     * two locations.
+     * Оценивает "стоимость" передвижения между двумя указанными точками
+     * Фактическая стоимость - это прямое расстояние между двумя точками
      **/
     private static float estimateTravelCost(Location currLoc, Location destLoc)
     {
